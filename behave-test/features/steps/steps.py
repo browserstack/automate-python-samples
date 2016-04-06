47684ff7689f1accfdeb975195aad0ff2dbad4a5
Nidhi Makhijani <nidhimj22@gmail.com>
2016-04-06 14:00:46 +0530

    restructure

--- behave-test/features/steps/steps.py
+++
@@ -1,7 +0,0 @@
-@when('visiting google')
-def step(context):
-    context.browser.get('http://www.google.com')
-
-@then('its title should be "Google"')
-def step(context):
-    assert context.browser.title == "Google"
--- lettuce-test/features/sample.feature
+++
@@ -1,5 +0,0 @@
-Feature: Go to google
-    Scenario: Visit Google
-        Given I go to "http://www.google.com/"
-            When field with name "q" is given "selenium"
-            Then "seleniumhq.org" is listed
