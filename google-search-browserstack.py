7cf59cef630b81ed58e64ce30b6ca59afff46652
serv-inc <serv-inc@users.noreply.github.com>
2019-05-27 14:34:46 +0200

    fix:minor typo fix
    pleaes -> please

--- google-search-browserstack.py
+++ google-search-browserstack.py
@@ -9,7 +9,7 @@ try:
     USERNAME = os.environ.get('BROWSERSTACK_USERNAME') or sys.argv[1]
     BROWSERSTACK_ACCESS_KEY = os.environ.get('BROWSERSTACK_ACCESS_KEY') or sys.argv[2]
 except IndexError:
-    print("Pleaes provide the username and browserstack access key as command line arguments.")
+    print("Please provide the username and browserstack access key as command line arguments.")
     sys.exit(1)
 
 driver = webdriver.Remote(
